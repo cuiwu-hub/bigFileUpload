f65fc7d8574a5bd0f0f24ea978237b54eaa07fb4
cuiwu <2946803165@qq.com>
2022-11-02 23:53:33 +0800

    first commit

---
+++ upload-servlet/src/main/java/com/example/uploadservlet/UploadServletApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadServletApplication.class, args);
+    }
+
+}
---
+++ upload-servlet/src/test/java/com/example/uploadservlet/UploadServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
